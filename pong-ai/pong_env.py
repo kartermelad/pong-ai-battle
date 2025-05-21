67228eacf6e86451843fd82ae865fb82d9b593eb
Karter <132957663+kartermelad@users.noreply.github.com>
2025-05-21 14:02:42 -0600

    pong_env basic completion

--- pong-ai/pong_env.py
+++ pong-ai/pong_env.py
@@ -3,11 +3,13 @@ import random
 import numpy as np
 
 class PongEnv(gym.Env):
+    
     def __init__(self):
         self.screen_width = 400
         self.screen_height = 300
         self.paddle_width = 10
         self.paddle_height = 60
+        self.paddle_speed = 8
         self.paddle1_y = self.screen_height / 2
         self.paddle2_y = self.screen_height / 2
         self.ball_x = 200
@@ -37,8 +39,6 @@ class PongEnv(gym.Env):
         return self._get_obs()
 
     def _get_obs(self):
-        paddle_speed = 8
-
         current_speed = np.sqrt(self.ball_vel_x ** 2 + self.ball_vel_y ** 2)
         if current_speed == 0:
             norm_ball_vel_x = 0
@@ -54,25 +54,71 @@ class PongEnv(gym.Env):
             norm_ball_vel_y,
             self.paddle1_y / (self.screen_height - self.paddle_height),
             self.paddle2_y / (self.screen_height - self.paddle_height),
-            self.paddle1_vel / paddle_speed,
-            self.paddle2_vel / paddle_speed
+            self.paddle1_vel / self.paddle_speed,
+            self.paddle2_vel / self.paddle_speed
         ], dtype=np.float32)
         return obs
     
     def step(self, action):
-        paddle_speed = 8
-
         if action == 1:
-            self.paddle1_vel = -paddle_speed
+            self.paddle1_vel = -self.paddle_speed
         elif action == 2:
-            self.paddle1_vel = paddle_speed
+            self.paddle1_vel = self.paddle_speed
         else:
             self.paddle1_vel = 0
-
         self.paddle1_y += self.paddle1_vel
         self.paddle1_y = max(0, min(self.paddle1_y, self.screen_height - self.paddle_height))
-        obs = self._get_obs()
+
+        paddle2_center = self.paddle2_y + self.paddle_height / 2
+        if self.ball_y < paddle2_center:
+            self.paddle2_vel = -self.paddle_speed
+        elif self.ball_y > paddle2_center:
+            self.paddle2_vel = self.paddle_speed
+        else:
+            self.paddle2_vel = 0
+        self.paddle2_y += self.paddle2_vel
+        self.paddle2_y = max(0, min(self.paddle2_y, self.screen_height - self.paddle_height))
+
+        self.ball_x += self.ball_vel_x
+        self.ball_y += self.ball_vel_y
+
+        if self.ball_y <= 0:
+            self.ball_y = 0
+            self.ball_vel_y *= -1
+        elif self.ball_y >= self.screen_height:
+            self.ball_y = self.screen_height
+            self.ball_vel_y *= -1
+
+        if self.ball_x <= self.paddle_width:
+            if self.paddle1_y <= self.ball_y <= self.paddle1_y + self.paddle_height:
+                self.ball_x = self.paddle_width 
+                self.ball_vel_x *= -1
+                self.paddle_hits += 1
+                self.ball_speed += 0.5
+                direction_x = 1 if self.ball_vel_x > 0 else -1
+                direction_y = 1 if self.ball_vel_y > 0 else -1
+                self.ball_vel_x = self.ball_speed * direction_x
+                self.ball_vel_y = self.ball_speed * direction_y
+            else:
+                reward = -1
+                done = True
+                return self._get_obs(), reward, done, {}
+
+        if self.ball_x >= self.screen_width - self.paddle_width:
+            if self.paddle2_y <= self.ball_y <= self.paddle2_y + self.paddle_height:
+                self.ball_x = self.screen_width - self.paddle_width
+                self.ball_vel_x *= -1
+                self.paddle_hits += 1
+                self.ball_speed += 0.5
+                direction_x = 1 if self.ball_vel_x > 0 else -1
+                direction_y = 1 if self.ball_vel_y > 0 else -1
+                self.ball_vel_x = self.ball_speed * direction_x
+                self.ball_vel_y = self.ball_speed * direction_y
+            else:
+                reward = 1
+                done = True
+                return self._get_obs(), reward, done, {}
+
         reward = 0
         done = False
-        info = {}
-        return obs, reward, done, info
+        return self._get_obs(), reward, done, {}
